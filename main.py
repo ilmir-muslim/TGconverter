d0a54c6e98c24fe2bd641ba650b7351786f4ec11
ilmir-muslim <ilmir_gf@mail.ru>
2022-05-25 14:55:29 +0200

    пытаеся разобраться с конвертере, следующий шаг выяснить как сделать конвертацию других валют

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import requests #модуль для обработки URL
 from bs4 import BeautifulSoup #Модуль для работы с HTML
 import time # Модуль для остановки программы
-import telebot # посмотреть какой модуль лучше подключить для работы с ботом, переделать код для телеграмм бота
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+#import telebot # посмотреть какой модуль лучше подключить для работы с ботом, переделать код для телеграмм бота
+
 # СЛЕДУЮЩИЙ ШАГ СОЕДЕНИТЬ ЭТОД КОД С КОДОМ БОТА КОНВЕРТЕРА!
 
 # Основной класс
 class Currency:
     #Ссылка на нужную страницу
     DOLLAR_RUB = 'https://www.google.com/search?sxsrf=ALeKk01NWm6viYijAo3HXYOEQUyDEDtFEw%3A1584716087546&source=hp&ei=N9l0XtDXHs716QTcuaXoAg&q=%D0%B4%D0%BE%D0%BB%D0%BB%D0%B0%D1%80+%D0%BA+%D1%80%D1%83%D0%B1%D0%BB%D1%8E&oq=%D0%B4%D0%BE%D0%BB%D0%BB%D0%B0%D1%80+&gs_l=psy-ab.3.0.35i39i70i258j0i131l4j0j0i131l4.3044.4178..5294...1.0..0.83.544.7......0....1..gws-wiz.......35i39.5QL6Ev1Kfk4'
-    #гЗаголовки для передачи  вместе с URL
+    #Заголовки для передачи  вместе с URL
     header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'}
     current_converted_price = 0
-    difference = 5 #Разница после которой будет отправлено сообщение на почту
+ #   difference = 5 #Разница после которой будет отправлено сообщение на почту
 
     def __init__(self):
         # Установка курса валюты при создании объекта
@@ -30,34 +30,17 @@ class Currency:
     # Проверка изменения валюты
     def check_currency(self):
         currency = float(self.get_currency_price().replace(",", "."))
-        if currency >= self.current_converted_price + self.difference:
-            print("Курс сильно вырос, может пора что-то делать?")
-            self.send_mail()
-        elif currency <= self.current_converted_price - self.difference:
-            print("Курс сильно упал, может пора что-то делать?")
-            self.send_mail()
-        print("Сейчас курс: 1 доллар = " + str(currency))
-        time.sleep(3) # Засыпание программы на 3 секунды
-        self.check_currency()
+        vibor = input('менять по курсу (введите: 1) или с наценкой (введите: 2): ')
+        if vibor == '1':
+            print('курс доллара к рублю: ' + str(currency))
+        elif vibor == '2':
+            procent = input("введите процент наценки: ")
+            svoikurs = currency + currency * (float(procent)/100)
+            print('курс с наценкой: ' + str(svoikurs))
+        else:
+            print('вы сделали не правильный выбор :(')
 
-    def send_mail(self):
-        server = smtplib.SMTP('smtp.gmail.com', 587)
-        server.ehlo()
-        server.starttls()
-        server.ehlo()
 
-        server.login('ilmirgilmiarov@yandex.ru', 'ak107gif')
-
-        subject = 'Currency mail'
-        body = 'Currency has been chaged!'
-        message = f'Subject: {subject}\n{body}'
-
-        server.sendmail(
-            'ilmirgilmiarov@yandex.ru',
-            'ilmir_gf@mail.ru',
-            message
-        )
-        server.quit()
 
 # Создание объекта и вызов метода
 currency = Currency()
